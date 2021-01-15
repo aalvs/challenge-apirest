c8167959095f42bdcf8dad8dfe7cfd2e263607d9
Andre Alves <andrealvesdeoliveira@outlook.com>
2021-01-15 20:42:52 -0300

    Ajustes no GenerationType

--- src/main/java/com/satback/satapi/models/Colaborador.java
+++ src/main/java/com/satback/satapi/models/Colaborador.java
@@ -5,9 +5,10 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name="tb_pessoas")
@@ -17,7 +18,9 @@ public class Colaborador implements Serializable {
 
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
 	private long id;
 	
 	@Column(nullable = false, length = 20)
